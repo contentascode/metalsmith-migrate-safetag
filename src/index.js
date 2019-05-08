25897e3efc873436483fb0718d6c4e9e916cc1f3
Jun Matsushita <jun@iilab.org>
2019-05-08 17:34:50 +0200

    Add additional metadata to migration

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-const debug = require('debug')('metalsmith:migrate-safetag');
-const minimatch = require('minimatch');
-const _ = require('lodash');
-const trimNewlines = require('trim-newlines');
-const fs = require('fs');
-const path = require('path');
+const debug = require('debug')('metalsmith:migrate-safetag')
+const minimatch = require('minimatch')
+const _ = require('lodash')
+const trimNewlines = require('trim-newlines')
+const fs = require('fs')
+const path = require('path')
 
 /**
  * Expose `plugin`.
  */
 
-module.exports = plugin;
+module.exports = plugin
 
 /**
  * Metalsmith plugin to migrate SAFETAG content to the metalsmith approach.
@@ -29,24 +29,49 @@ function plugin(options) {
     images = false,
     document_matter = false,
     guides = false,
+    reporting = false,
     origin,
-    origin_path_prefix
-  } =
-    options || {};
+    origin_path_prefix,
+  } = options || {}
 
   return function(files, metalsmith, done) {
     //
-    const footnotes = ':[](../references/footnotes.md)';
+    const footnotes = ':[](../references/footnotes.md)'
 
     // First pass reducer to group exercises files in one activity and prepare for transform pass.
     const walk = (acc, file, key) => {
       // Process activities.
       if (activities && minimatch(key, 'exercises/*/**.md')) {
         // Default to empty contents.
-        const contents = { contents: new Buffer('') };
+        const contents = { contents: new Buffer('') }
+
+        const metadata = minimatch(key, 'exercises/*/index.md')
+          ? {
+              metadata: {
+                ...(file.Authors ? { authors: file.Authors } : null),
+                ...(file.Approach
+                  ? { approach_category: file.Approach }
+                  : null),
+                ...(file.Org_size_under
+                  ? { org_size_under: file.Org_size_under }
+                  : null),
+                ...(file.Remote_options
+                  ? { remote_options: file.Remote_options }
+                  : null),
+                ...(file.Skills_required
+                  ? { skills_required: file.Skills_required }
+                  : null),
+                ...(file.Time_required_minutes
+                  ? { time_required_minutes: file.Time_required_minutes }
+                  : null),
+              },
+            }
+          : null
 
         // Get index content
-        const index = minimatch(key, 'exercises/*/index.md') ? file.contents.toString() : null;
+        const index = minimatch(key, 'exercises/*/index.md')
+          ? file.contents.toString()
+          : null
 
         // Sanity check: Verify that activity index structure is as expected
         const fields = [
@@ -55,43 +80,67 @@ function plugin(options) {
           'materials_needed',
           'operational_security',
           'instructions',
-          'recommendations'
-        ];
+          'recommendations',
+          'output',
+        ]
 
-        const includes_regexp = /^!INCLUDE\s"(.*)\.md"/gm;
+        const includes_regexp = /^!INCLUDE\s"(.*)\.md"/gm
 
-        let match;
-        const matches = [];
+        let match
+        const matches = []
 
-        while ((match = includes_regexp.exec(index))) matches.push(match[1]);
+        while ((match = includes_regexp.exec(index))) matches.push(match[1])
 
         matches.forEach(match => {
           if (!fields.includes(match))
-            console.warn('Unexpected transclusion ' + match + ' in activity index file ' + key);
-        });
+            console.warn(
+              'Unexpected transclusion ' +
+                match +
+                ' in activity index file ' +
+                key
+            )
+        })
 
         // Match title
 
-        const title = index && index.match(/^####\s(.*)$/m) ? { title: index.match(/^####\s(.*)$/m)[1] } : null;
+        const title =
+          index && index.match(/^####\s(.*)$/m)
+            ? { title: index.match(/^####\s(.*)$/m)[1] }
+            : null
 
-        const processTransclusions = str => trimNewlines(str.toString().replace(includes_regexp, ':[]($1.md)'));
+        const processTransclusions = str =>
+          trimNewlines(str.toString().replace(includes_regexp, ':[]($1.md)'))
 
         // Sanity check: Check if transclusion destinations exist.
 
-        const transclusionRE = /:\[\]\((.*)\)/gm;
-        let destLinks;
+        const transclusionRE = /:\[\]\((.*)\)/gm
+        let destLinks
 
-        while ((destLinks = transclusionRE.exec(processTransclusions(file.contents))) !== null) {
+        while (
+          (destLinks = transclusionRE.exec(
+            processTransclusions(file.contents)
+          )) !== null
+        ) {
           try {
             fs.openSync(
-              path.join(metalsmith.source(), 'exercises', key.split('exercises/')[1].split('/')[0], destLinks[1]),
+              path.join(
+                metalsmith.source(),
+                'exercises',
+                key.split('exercises/')[1].split('/')[0],
+                destLinks[1]
+              ),
               fs.constants.O_RDONLY
-            );
+            )
           } catch (e) {
             console.log(
               `Missing transclusion destination in ${key}:`,
-              path.join(metalsmith.source(), 'exercises', key.split('exercises/')[1].split('/')[0], destLinks[1])
-            );
+              path.join(
+                metalsmith.source(),
+                'exercises',
+                key.split('exercises/')[1].split('/')[0],
+                destLinks[1]
+              )
+            )
           }
         }
 
@@ -99,32 +148,36 @@ function plugin(options) {
 
         const summary = minimatch(key, 'exercises/*/summary.md')
           ? { summary: processTransclusions(file.contents) }
-          : null;
+          : null
 
         const approach = minimatch(key, 'exercises/*/approach.md')
           ? { approach: processTransclusions(file.contents) }
-          : null;
+          : null
         const materials = minimatch(key, 'exercises/*/materials_needed.md')
           ? { materials: processTransclusions(file.contents) }
-          : null;
+          : null
         const opsec = minimatch(key, 'exercises/*/operational_security.md')
           ? { opsec: processTransclusions(file.contents) }
-          : null;
+          : null
         const instructions = minimatch(key, 'exercises/*/instructions.md')
           ? { instructions: processTransclusions(file.contents) }
-          : null;
+          : null
         const recommendations = minimatch(key, 'exercises/*/recommendations.md')
           ? { recommendations: processTransclusions(file.contents) }
-          : null;
+          : null
+        const output = minimatch(key, 'exercises/*/output.md')
+          ? { output: processTransclusions(file.contents) }
+          : null
 
         const id = {
           id: key
             .split('exercises/')[1]
             .split('/')[0]
-            .replace(/_/g, '-')
-        };
-        const activity = 'activities/' + id.id + '.md';
-
+            .replace(/_/g, '-'),
+        }
+        const activity = 'activities/' + id.id + '.md'
+        // console.log('walk.activities.id', activity)
+        // console.log('walk.activities.metadata', metadata)
         // Assemble single activity file with metadata fields for second pass.
         return {
           ...acc,
@@ -138,35 +191,43 @@ function plugin(options) {
             ...materials,
             ...opsec,
             ...instructions,
+            ...output,
             ...recommendations,
-            origin_path: origin_path_prefix + key
-          }
-        };
-      } else if (methods && (minimatch(key, 'methods/*.md') || minimatch(key, 'methods/*/*.md'))) {
+            ...metadata,
+            origin_path: origin_path_prefix + key,
+          },
+        }
+      } else if (
+        methods &&
+        (minimatch(key, 'methods/*.md') || minimatch(key, 'methods/*/*.md'))
+      ) {
         // Replace transclusion links and remove Activities heading
         const contents = file.contents
           .toString()
           .replace(/^!INCLUDE "(.*)"\W?$/gm, ':[]($1)')
-          .replace(/\/exercises\//g, '/activities/');
+          .replace(/\/exercises\//g, '/activities/')
 
         // Activities are listed in index.guide.md
         // For now, instead of scraping it, reuse taxonomy in toolkit pipeline to display activity browser.
 
         // Sanity check: Check if transclusion destinations exist.
 
-        const transclusionRE = /:\[\]\((.*)\)/gm;
-        let destLinks;
+        const transclusionRE = /:\[\]\((.*)\)/gm
+        let destLinks
 
         while ((destLinks = transclusionRE.exec(contents)) !== null) {
           // We skip activities as they are linked to methods via the taxonomy
           if (!destLinks[1].includes('/activities/')) {
             try {
-              fs.openSync(path.join(metalsmith.source(), 'methods', destLinks[1]), fs.constants.O_RDONLY);
+              fs.openSync(
+                path.join(metalsmith.source(), 'methods', destLinks[1]),
+                fs.constants.O_RDONLY
+              )
             } catch (e) {
               console.log(
                 `Missing transclusion destination in ${key}:`,
                 path.join(metalsmith.source(), 'methods', destLinks[1])
-              );
+              )
             }
           }
         }
@@ -174,7 +235,9 @@ function plugin(options) {
         // Match title
 
         const title =
-          contents && contents.match(/^####\s(.*)$/m) ? { title: contents.match(/^####\s(.*)$/m)[1] } : null;
+          contents && contents.match(/^##\s(.*)$/m)
+            ? { title: contents.match(/^##\s(.*)$/m)[1] }
+            : null
 
         // Assemble single activity file with metadata fields for second pass.
         // Create ${method}.md file as target for toolkit display and leave ${method}.guide.md for guide preview.
@@ -186,7 +249,7 @@ function plugin(options) {
             id: key.replace('.guide.md', '.md'),
             ...title,
             layout: 'method.md',
-            origin_path: origin_path_prefix + key
+            origin_path: origin_path_prefix + key,
           },
           [key]: {
             ...file,
@@ -194,19 +257,23 @@ function plugin(options) {
             id: key,
             ...title,
             layout: 'method.md',
-            origin_path: origin_path_prefix + key
-          }
-        };
+            origin_path: origin_path_prefix + key,
+          },
+        }
       } else if (document_matter && minimatch(key, 'document_matter/**/*.md')) {
         // Replace transclusion links
-        const contents = file.contents.toString().replace(/^!INCLUDE "(.*)"\W?$/gm, ':[]($1)');
+        const contents = file.contents
+          .toString()
+          .replace(/^!INCLUDE "(.*)"\W?$/gm, ':[]($1)')
 
         // TODO:Sanity checks
 
         // Match title
 
         const title =
-          contents && contents.match(/^####\s(.*)$/m) ? { title: contents.match(/^####\s(.*)$/m)[1] } : null;
+          contents && contents.match(/^##\s(.*)$/m)
+            ? { title: contents.match(/^##\s(.*)$/m)[1] }
+            : null
 
         // Assemble single activity file with metadata fields for second pass.
         return {
@@ -217,19 +284,25 @@ function plugin(options) {
             id: key,
             ...title,
             layout: 'page.md',
-            origin_path: origin_path_prefix + key
-          }
-        };
-      } else if (references && minimatch(key, 'references/*.md') && !minimatch(key, 'references/footnotes.md')) {
+            origin_path: origin_path_prefix + key,
+          },
+        }
+      } else if (
+        references &&
+        minimatch(key, 'references/*.md') &&
+        !minimatch(key, 'references/footnotes.md')
+      ) {
         // TODO: Check external links and download for offline use.
-        const contents = file.contents.toString();
+        const contents = file.contents.toString()
 
         // TODO:Sanity checks
 
         // Match title
 
         const title =
-          contents && contents.match(/^####\s(.*)$/m) ? { title: contents.match(/^####\s(.*)$/m)[1] } : null;
+          contents && contents.match(/^####\s(.*)$/m)
+            ? { title: contents.match(/^####\s(.*)$/m)[1] }
+            : null
 
         // Assemble single activity file with metadata fields for second pass.
         return {
@@ -241,17 +314,17 @@ function plugin(options) {
             ...title,
             description: '',
             layout: 'reference.md',
-            origin_path: origin_path_prefix + key
-          }
-        };
+            origin_path: origin_path_prefix + key,
+          },
+        }
       } else if (references && minimatch(key, 'references/footnotes.md')) {
         // Footnotes need to be transformed to prevent confusion with transclusion syntax
 
-        const contents = file.contents.toString().replace(/(\[.*\]:)/g, '$1 ');
+        const contents = file.contents.toString().replace(/(\[.*\]:)/g, '$1 ')
 
         // TODO:Sanity checks
 
-        const title = 'footnotes';
+        const title = 'footnotes'
 
         return {
           ...acc,
@@ -262,9 +335,9 @@ function plugin(options) {
             ...title,
             description: '',
             layout: 'reference.md',
-            origin_path: origin_path_prefix + key
-          }
-        };
+            origin_path: origin_path_prefix + key,
+          },
+        }
       } else if (images && minimatch(key, 'images/**/*.*')) {
         // Move images to methods folder for now.
         // const move = 'methods/' + key;
@@ -275,12 +348,12 @@ function plugin(options) {
         return {
           ...acc,
           [key]: {
-            ...file
-          }
-        };
+            ...file,
+          },
+        }
       } else if (guides && minimatch(key, 'index.guide.md')) {
         // Move guide to index.guide.md.
-        const move = 'index.guide.md';
+        const move = 'index.guide.md'
 
         const contents = file.contents
           .toString()
@@ -291,7 +364,7 @@ function plugin(options) {
             (_, slug) => ':[](./activities/' + slug.replace(/_/g, '-') + '.md)'
           )
           // Add footnotes
-          .concat('\n:[](references/footnotes.md)');
+          .concat('\n:[](references/footnotes.md)')
 
         // Parse methods activities which are included in the index.guide
         // and reference them in order to add them to the method file.
@@ -304,77 +377,112 @@ function plugin(options) {
             ...file,
             contents: new Buffer(contents),
             layout: 'guide.md',
-            origin_path: origin_path_prefix + key
-          }
-        };
+            origin_path: origin_path_prefix + key,
+          },
+        }
       }
-      return acc;
-    };
+      return acc
+    }
 
     // Second pass transform.
     const transform = (file, key) => {
-      // TODO: Refactor to separate transform on all from special cases
-
       // Add footnotes file to activities and methods (only the used ones will be displayed by pandoc)
-
-      if (activities && key === 'activities/check-user-browser-vulns.md') {
-        return {
-          ...file,
-          contents: new Buffer(file.contents.toString().replace(/\\newpage/gm, '\n---')),
-          title: 'Check user browser vulnerabilities',
-          description: 'Outdated Java browser plugins',
-          special: files['exercises/check_user_browser_vulns/browser_java_plugin.md'].contents.toString(),
-          origin,
-          footnotes
-        };
-      } else if (activities && minimatch(key, 'activities/**/*.md')) {
+      if (activities && minimatch(key, 'activities/**/*.md')) {
         const description = file.summary
           ? trimNewlines(file.summary)
               .substring(0, 120)
               .replace(/^##(.*)$/m, '')
               .replace(/\r?\n|\r/g, '')
+              .replace(/!\[.*\]\((\S*)\s?(\S?)\)/g, '')
+              .replace(/"/g, '')
               .split(' ')
               .slice(0, -1)
               .join(' ') + '...'
-          : file.title || '';
-        debug('description', description);
+          : file.title || ''
+        debug('description', description)
 
         return {
           ...file,
-          contents: new Buffer(file.contents.toString().replace(/\\newpage/gm, '\n---')),
+          ...file.metadata,
+          contents: new Buffer(
+            file.contents.toString().replace(/\\newpage/gm, '\n---')
+          ),
           description,
           origin,
-          footnotes
-        };
+          footnotes,
+        }
       } else if (methods && minimatch(key, 'methods/*.md')) {
+        const summary =
+          files[`${key.replace('.md', '')}/summary.md`] &&
+          files[`${key.replace('.md', '')}/summary.md`].contents.toString()
+
+        const description = summary
+          ? trimNewlines(summary)
+              .substring(0, 240)
+              .replace(/^###(.*)$/m, '')
+              .replace(/\r?\n|\r/g, '')
+              .replace(/!\[.*\]\((\S*)\s?(\S?)\)/g, '')
+              .replace(/"/g, '')
+              .split(' ')
+              .slice(0, -1)
+              .join(' ') + '...'
+          : file.title || ''
+        debug('description', description)
+
         return {
           ...file,
-          contents: new Buffer(file.contents.toString().replace(/\\newpage/gm, '\n---') + '\n' + files.activities),
+          contents: new Buffer(
+            file.contents.toString().replace(/\\newpage/gm, '\n---') +
+              '\n' +
+              files.activities
+          ),
+          description,
           origin,
-          footnotes
-        };
+          footnotes,
+        }
       }
+
+      const description = file.contents.toString()
+        ? trimNewlines(file.contents.toString())
+            .substring(0, 240)
+            .replace(/^###(.*)$/m, '')
+            .replace(/^##(.*)$/m, '')
+            .replace(/\r?\n|\r/g, '')
+            .replace(/!\[.*\]\((\S*)\s?(\S?)\)/g, '')
+            .replace(/"/g, '')
+            .replace(/\*/g, '')
+            .split(' ')
+            .slice(0, -1)
+            .join(' ') + '...'
+        : file.title || ''
+      debug('description', description)
+
       return {
         ...file,
-        contents: new Buffer(file.contents.toString().replace(/\\newpage/gm, '\n---')),
-        origin
-      };
-    };
+        contents: new Buffer(
+          file.contents.toString().replace(/\\newpage/gm, '\n---')
+        ),
+        description,
+        origin,
+      }
+    }
 
-    const reduced = _.reduce(files, walk, {});
-    const results = _.mapValues(reduced, transform);
+    const reduced = _.reduce(files, walk, {})
+    const results = _.mapValues(reduced, transform)
+
+    debug('results', results)
 
     // Delete original files object.
     Object.keys(files).forEach(key => {
-      delete files[key];
-    });
+      delete files[key]
+    })
 
     // Restore files object with migration results
     // And add origin metatag for upstream edit link
     Object.keys(results).forEach(key => {
-      files[key] = { ...results[key] };
-    });
+      files[key] = { ...results[key] }
+    })
 
-    done();
-  };
+    done()
+  }
 }
